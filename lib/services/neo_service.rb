a98f3edd1d447058050fe06a57e24e7874351ae3
tech-a49 <tom.e.beckett@gmail.com>
2018-10-19 14:51:17 +0100

    tests

--- lib/services/neo_service.rb
+++ lib/services/neo_service.rb
@@ -11,4 +11,96 @@ class NeoService
     @api_key = "6982JbtUDAuS4ZJ1JdsHZAQ767pXiDRVM3ajjCks"
     @neo = JSON.parse(self.class.get("start_date=#{start_date}&end_date=#{end_date}&api_key=#{@api_key}").body)
   end
+
+  def get_neo_keys
+    @neo["near_earth_objects"].each_key do |key|
+      unless key.is_a? Array
+        return true
+      end
+    end
+    return false
+  end
+
+  def get_each_neo
+    @neo["near_earth_objects"].each_key do |key|
+      @neo["near_earth_objects"][key].each do |object|
+        unless object.is_a? Hash
+          return false
+        end
+      end
+    end
+    return true
+  end
+
+  def get_each_neo_key k, v
+    @neo["near_earth_objects"].each_key do |key|
+      @neo["near_earth_objects"][key].each do |object|
+        unless object[k].is_a? v
+          return false
+        end
+      end
+    end
+    return true
+  end
+
+  def neo_key_bool k
+    @neo["near_earth_objects"].each_key do |key|
+      @neo["near_earth_objects"][key].each do |object|
+        unless [true, false].include? object[k]
+          return false
+        end
+      end
+    end
+    return true
+  end
+
+  def get_estimated_diameter_keys
+    @neo["near_earth_objects"].each_key do |key|
+      @neo["near_earth_objects"][key].each do |object|
+        unless object["estimated_diameter"].keys == ["kilometers","meters","miles", "feet"]
+          return false
+        end
+      end
+    end
+    return true
+  end
+
+  def min_max_diameter
+    @neo["near_earth_objects"].each_key do |key|
+      @neo["near_earth_objects"][key].each do |object|
+        object["estimated_diameter"].each_key do |k|
+          unless object["estimated_diameter"][k]["estimated_diameter_min"] <= object["estimated_diameter"][k]["estimated_diameter_max"]
+            return false
+          end
+        end
+      end
+    end
+    return true
+  end
+
+  def close_approach k, v
+    @neo["near_earth_objects"].each_key do |key|
+      @neo["near_earth_objects"][key].each do |object|
+        object["close_approach_data"].each do |data|
+          unless data[k].is_a? v
+            return false
+          end
+        end
+      end
+    end
+    return true
+  end
+
+  def close_approach_units k, units
+    @neo["near_earth_objects"].each_key do |key|
+      @neo["near_earth_objects"][key].each do |object|
+        object["close_approach_data"].each do |data|
+          unless data[k].keys == units
+            return false
+          end
+        end
+      end
+    end
+    return true
+  end
 end
--- spec/neo_service_spec.rb
+++ spec/neo_service_spec.rb
@@ -35,5 +35,104 @@ describe 'Nasa NEO' do
     it "should get element count as an integer " do
       expect(@neo.neo["element_count"]).to be_an Integer
     end
+
+    it "should get near earth objects as a hash" do
+      expect(@neo.neo["near_earth_objects"]).to be_an Hash
+    end
+
+    it "should have two keys (for days 2018-10-18 and 2018-10-19)" do
+      expect(@neo.neo["near_earth_objects"].keys.length).to be >= 1
+    end
+
+    it "should have keys as arrays" do
+      expect(@neo.get_neo_keys).to eq true
+    end
+
+    it "should return each neo on a date as a hash" do
+      expect(@neo.get_each_neo)
+    end
+
+    it "should get each neo with links key as a hash" do
+      expect(@neo.get_each_neo_key "links", Hash).to eq true
+    end
+
+    it "should get each neo with id key as a string" do
+      expect(@neo.get_each_neo_key "id", String).to eq true
+    end
+
+    it "should get each neo with neo_reference_id key as a string" do
+      expect(@neo.get_each_neo_key "neo_reference_id", String).to eq true
+    end
+
+    it "should get each neo with name key as a String" do
+      expect(@neo.get_each_neo_key "name", String).to eq true
+    end
+
+    it "should get each neo with nasa_jpl_url key as a string" do
+      expect(@neo.get_each_neo_key "nasa_jpl_url", String).to eq true
+    end
+
+    it "should get each neo with absolute_magnitude_h key as a number" do
+      expect(@neo.get_each_neo_key "absolute_magnitude_h", Numeric).to eq true
+    end
+
+    it "should get each neo with estimated_diameter key as a hash" do
+      expect(@neo.get_each_neo_key "estimated_diameter", Hash).to eq true
+    end
+
+    it "should get each neo with is_potentially_hazardous_asteroid key as a boolean" do
+      expect(@neo.neo_key_bool "is_potentially_hazardous_asteroid").to eq true
+    end
+
+    it "should get each neo with close_approach_data key as a array" do
+      expect(@neo.get_each_neo_key "close_approach_data", Array).to eq true
+    end
+
+    it "should get each neo with is_sentry_object key as a boolean" do
+      expect(@neo.neo_key_bool "is_sentry_object").to eq true
+    end
+
+
+  end
+
+  context "estimated_diameter" do
+    it "should have km, meters, miles, and feet " do
+      expect(@neo.get_estimated_diameter_keys).to be true
+    end
+
+    it "should have min diamter less than max diameter" do
+      expect(@neo.min_max_diameter).to eq true
+    end
+  end
+
+  context "close approach data" do
+    it "should have a close approach date to be a string" do
+      expect(@neo.close_approach "close_approach_date", String).to eq true
+    end
+
+    it "should have a epoch_date_close_approach to be a Integer" do
+      expect(@neo.close_approach "epoch_date_close_approach", Integer).to eq true
+    end
+
+    it "should have a relative_velocity to be a Hash" do
+      expect(@neo.close_approach "relative_velocity", Hash).to eq true
+    end
+
+    it "should have a miss_distance to be a Hash" do
+      expect(@neo.close_approach "miss_distance", Hash).to eq true
+    end
+
+    it "should have a orbiting_body to be a String" do
+      expect(@neo.close_approach "orbiting_body", String).to eq true
+    end
+
+    it "should have relative velocity in kilometers_per_second, kilometers_per_hour, and miles_per_hour " do
+      expect(@neo.close_approach_units "relative_velocity", ["kilometers_per_second", "kilometers_per_hour", "miles_per_hour"]).to eq true
+    end
+
+    it "should have miss_distance in astronomical, lunar, kilometers and miles " do
+      expect(@neo.close_approach_units "miss_distance", ["astronomical","lunar", "kilometers", "miles"]).to eq true
+    end
+
   end
 end
